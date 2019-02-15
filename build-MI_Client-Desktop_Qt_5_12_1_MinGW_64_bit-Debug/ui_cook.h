1d32524bdf4318c7a4e2925d771fd9285a4c8ff1
DeNoctis <stardustdragon951@gmail.com>
2019-02-15 13:45:26 +0200

    Change apl

--- build-MI_Client-Desktop_Qt_5_12_1_MinGW_64_bit-Debug/ui_cook.h
+++ build-MI_Client-Desktop_Qt_5_12_1_MinGW_64_bit-Debug/ui_cook.h
@@ -11,7 +11,6 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QLabel>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QWidget>
 
@@ -20,20 +19,42 @@ QT_BEGIN_NAMESPACE
 class Ui_Cook
 {
 public:
-    QLabel *label;
     QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_5;
+    QWidget *widget;
+    QPushButton *pushButton_6;
 
     void setupUi(QWidget *Cook)
     {
         if (Cook->objectName().isEmpty())
             Cook->setObjectName(QString::fromUtf8("Cook"));
-        Cook->resize(400, 300);
-        label = new QLabel(Cook);
-        label->setObjectName(QString::fromUtf8("label"));
-        label->setGeometry(QRect(20, 60, 181, 81));
+        Cook->resize(829, 577);
         pushButton = new QPushButton(Cook);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(10, 10, 75, 23));
+        pushButton->setGeometry(QRect(10, 50, 151, 23));
+        pushButton_2 = new QPushButton(Cook);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(10, 80, 151, 23));
+        pushButton_3 = new QPushButton(Cook);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+        pushButton_3->setGeometry(QRect(10, 300, 151, 23));
+        pushButton_4 = new QPushButton(Cook);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+        pushButton_4->setGeometry(QRect(10, 330, 151, 23));
+        pushButton_5 = new QPushButton(Cook);
+        pushButton_5->setObjectName(QString::fromUtf8("pushButton_5"));
+        pushButton_5->setGeometry(QRect(750, 10, 71, 23));
+        widget = new QWidget(Cook);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(170, 50, 641, 511));
+        pushButton_6 = new QPushButton(Cook);
+        pushButton_6->setObjectName(QString::fromUtf8("pushButton_6"));
+        pushButton_6->setEnabled(true);
+        pushButton_6->setGeometry(QRect(780, 60, 21, 23));
+        pushButton_6->setLayoutDirection(Qt::LeftToRight);
 
         retranslateUi(Cook);
 
@@ -43,8 +64,12 @@ public:
     void retranslateUi(QWidget *Cook)
     {
         Cook->setWindowTitle(QApplication::translate("Cook", "Form", nullptr));
-        label->setText(QApplication::translate("Cook", "TextLabel", nullptr));
-        pushButton->setText(QApplication::translate("Cook", "PushButton", nullptr));
+        pushButton->setText(QApplication::translate("Cook", "\320\237\320\276\321\201\320\274\320\276\321\202\321\200\320\265\321\202\321\214 \320\261\320\273\321\216\320\264\320\260", nullptr));
+        pushButton_2->setText(QApplication::translate("Cook", "\320\237\320\276\321\201\320\274\320\276\321\202\321\200\320\265\321\202\321\214 \320\270\320\275\320\263\321\200\320\270\320\264\320\270\320\265\320\275\321\202\321\213", nullptr));
+        pushButton_3->setText(QApplication::translate("Cook", "\320\241\320\276\320\267\320\264\320\260\321\202\321\214 \320\274\320\265\320\275\321\216", nullptr));
+        pushButton_4->setText(QApplication::translate("Cook", "\320\237\320\276\321\201\320\274\320\276\321\202\321\200\320\265\321\202\321\214 \320\277\321\200\320\276\321\210\320\273\321\213\320\265 \320\274\320\265\320\275\321\216", nullptr));
+        pushButton_5->setText(QApplication::translate("Cook", "\320\237\320\265\321\200\320\265\320\267\320\260\320\271\321\202\320\270", nullptr));
+        pushButton_6->setText(QApplication::translate("Cook", "\320\245", nullptr));
     } // retranslateUi
 
 };
